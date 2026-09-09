3ee74f1b8ef77dc30688100aedc4621898d1fd71
Mukunthan V <mukunthan2130@gmail.com>
2026-09-09 21:49:11 +0530

    comeback

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -6,16 +6,16 @@ import transform
 
 def test_transform():
 
-    # data = {
-    #     'id': [1, 2, 3],
-    #     'title': ['Post 1', 'Post 2', 'Post 3'],
-    #     'content': ['Content 1', 'Content 2', 'Content 3']
-    # }
     data = {
-        "userId": [1, 2],
-        "id": [101, 102],
-        "title": ["A", "B"]
+        'id': [1, 2, 3],
+        'title': ['Post 1', 'Post 2', 'Post 3'],
+        'content': ['Content 1', 'Content 2', 'Content 3']
     }
+    # data = {
+    #     "userId": [1, 2],
+    #     "id": [101, 102],
+    #     "title": ["A", "B"]
+    # }
     df = pd.DataFrame(data)
     
     df.to_json("test_data.json")
